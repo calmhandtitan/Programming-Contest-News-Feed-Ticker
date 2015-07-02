9707dc411e7b65eaba3ecc44007550a3e00934f4
Chandan Mittal <chandan.iiitdmj@gmail.com>
2015-07-03 01:24:36 +0530

    added runtime proxy Configuration

--- main.py
+++ main.py
@@ -2,9 +2,24 @@ import os
 import codechef
 import codeforces
 import checkfile
+from bcolors import *
+
+def getProxy():
+	print bColors.HEADER + 'Do you use a proxy server for accessing Internet?(y or n)' + bColors.ENDC
+	response = raw_input().strip().lower()
+	proxyConfig = ""
+	if 'y' in response:
+		print bColors.OKBLUE + 'Enter HOST:PORT' + bColors.ENDC
+		host, port = raw_input().strip().split(':')
+		print bColors.OKBLUE + 'Enter Username:Password' + bColors.ENDC
+		uname, pswd = raw_input().strip().split(':')
+		proxyConfig = "http://" + uname + ":" + pswd + "@" + host + ":" + port
+	return proxyConfig
+	
 
 if __name__ == "__main__":
-	CC = codechef.CodeChef()
-	CF = codeforces.CodeForces()
+	proxyConfig = getProxy()	
+	CC = codechef.CodeChef(proxyConfig)
+	CF = codeforces.CodeForces(proxyConfig)
 	if os.path.exists("news.txt"):
 		checkFile = checkfile.CheckFile()
